2ffa2b7b08480abd38e79663febf2b626f124149
branch10480 <toshiharu.imaeda@gmail.com>
2022-12-18 14:28:22 +0900

    Initial Commit

---
+++ UsingLottie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingLottie
+//
+//  Created by Toshiharu Imaeda on 2022/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
